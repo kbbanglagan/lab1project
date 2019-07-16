88a72cff95665af5eebb5e7da357ff8e68bf0af2
kbbanglagan <52182417+kbbanglagan@users.noreply.github.com>
2019-07-15 17:02:58 -0700

    Add files via upload

---
+++ lab1/index.php
@@ -0,0 +1,34 @@
+<!doctype html>	
+
+<html>
+
+<head>
+
+<title> Introduction </title>
+
+</head>
+
+<body>
+<body bgcolor="skyblue">
+<br>
+
+<img src="project.jpg" "width="400px" height="300px">
+<h1> Kevin Josef B. Banglagan </h1>
+
+<p> I'm Kevin Banglagan. I'm 18 years old. I'm from Pasay City. I graduated from St. Mary's Academy - Pasay City(JHS/SHS) </p>
+
+<p> I'm more as a computer tech guy. I spend a lot of time on computer or playing computer games with friends. I like building, cleaning computer and
+help friends who are in need creating their own computer setup. I want to travel to Korea, Japan, US to experience what's like being there because
+I've been to Canada when was I young. I followed tech guys on social media platforms like (Linus Tech Tips, Bitwit, JayzTwoCents, GamerNexus, etc..).
+I tend to eat a lot of food when I'm alone while using computer. 
+I got the inspiration on computers when I was grade 5. My first computer was when I'm 11 years old with my brother-in-law.
+He help me building my first computer setup and he was an I.T too, that makes me choose I.T as my course. </p>
+
+<p> My expectation on this class is to help me creating UI/UX, techniques and have knowledge about the subject to make me a better I.T student. </p>
+
+<img src="tenor.gif" />
+
+</body
+
+
+</html>
